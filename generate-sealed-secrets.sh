2ce5ee74f077c9fafeb2813c45de97a2c0ac2b31
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-07-26 15:12:36 +0900

    fix: sealed secrets install
    Co-authored-by: Jiho Lee <optional.int@kakao.com>
    Co-authored-by: 신일섭 <shinilseop12@gmail.com>

--- generate-sealed-secrets.sh
+++ generate-sealed-secrets.sh
@@ -1,4 +1,4 @@
 kubectl create secret generic application-secret --dry-run=client --from-env-file=./secrets.env -o json > secrets.json
-kubeseal --controller-name sealed-secrets --controller-namespace default --scope cluster-wide -o yaml < secrets.json > sealed-secret.yaml
+kubeseal --controller-name so1s-sealed-secrets --controller-namespace sealed-secrets --scope cluster-wide -o yaml < secrets.json > sealed-secret.yaml
 
 mv sealed-secret.yaml ./charts/backend/templates/

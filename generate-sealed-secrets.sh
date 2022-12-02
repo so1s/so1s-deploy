ead95c3ff7eed543c0a6a9a5c325ac14bee50346
Jiho Lee <optional.int@kakao.com>
2022-12-03 07:39:32 +0900

    chore: remove docker pull secret
    Co-authored-by: NEM-NE <dlatqdlatq@mju.ac.kr>
    Co-authored-by: shinilseop <shinilseop12@gmail.com>

--- generate-sealed-secrets.sh
+++ generate-sealed-secrets.sh
@@ -4,7 +4,4 @@ kubeseal --controller-name so1s-sealed-secrets --controller-namespace sealed-sec
 kubectl create secret generic default-registry --dry-run=client --from-env-file=./registry.env -n backend -o json > registry-secret.json
 kubeseal --controller-name so1s-sealed-secrets --controller-namespace sealed-secrets --scope cluster-wide -o yaml < registry-secret.json > registry-secret.yaml
 
-kubectl create secret docker-registry so1s --dry-run=client --from-file=.dockerconfigjson=./docker-config.json -o json > docker-pull-secret.json
-kubeseal --controller-name so1s-sealed-secrets --controller-namespace sealed-secrets --scope cluster-wide -o yaml < docker-pull-secret.json > docker-pull-secret.yaml
-
-mv sealed-secret.yaml docker-pull-secret.yaml registry-secret.yaml ./charts/backend/templates/
+mv sealed-secret.yaml registry-secret.yaml ./charts/backend/templates/

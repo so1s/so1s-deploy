4d50094be51e24b946d8ba50c986e8348aa4c6ac
Jiho Lee <optional.int@kakao.com>
2022-12-12 05:32:44 +0900

    feat: write install dependencies script

---
+++ install-dependencies.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p bash kubectl helm argocd
+
+set -x
+
+helm install argocd -n argocd -f argocd/dev-values.yaml argo/argo-cd --create-namespace --wait
+helm install external-dns -n kube-system -f charts/public/external-dns/dev-values.yaml charts/public/external-dns --create-namespace --wait
+
+argoCDPassword=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo)
+rm -f ./argocd-password
+echo $argoCDPassword > ./argocd-password
+
+kubectl apply -f ./project/project-dev.yaml --wait
+kubectl apply -f ./apps/dev/app-sealed-secrets.yaml --wait

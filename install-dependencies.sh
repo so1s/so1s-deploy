8bf077018ec41393448e451ec88fd28993100be0
Jiho Lee <optional.int@kakao.com>
2022-12-12 05:39:28 +0900

    feat: install alb

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -5,6 +5,7 @@ set -x
 
 helm install argocd -n argocd -f argocd/dev-values.yaml argo/argo-cd --create-namespace --wait
 helm install external-dns -n kube-system -f charts/public/external-dns/dev-values.yaml charts/public/external-dns --create-namespace --wait
+helm install alb -n kube-system -f charts/public/aws-load-balancer-controller/dev-values.yaml aws/aws-load-balancer-controller --create-namespace --wait
 
 argoCDPassword=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo)
 rm -f ./argocd-password

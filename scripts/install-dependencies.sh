0fb59cdd3e433c9b520ce255d44896631eda224f
Jiho Lee <optional.int@kakao.com>
2022-12-12 18:14:31 +0900

    feat: install helm repos for install dependencies

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -3,9 +3,15 @@
 
 set -x
 
-helm install argocd -n argocd -f argocd/dev-values.yaml argo/argo-cd --create-namespace --wait
-helm install external-dns -n kube-system -f charts/public/external-dns/dev-values.yaml charts/public/external-dns --create-namespace --wait
-helm install alb -n kube-system -f charts/public/aws-load-balancer-controller/dev-values.yaml aws/aws-load-balancer-controller --create-namespace --wait
+cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && cd ..
+
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo add aws https://aws.github.io/eks-charts
+
+helm install argocd -n argocd -f ./argocd/dev-values.yaml argo/argo-cd --create-namespace --wait
+helm install external-dns -n kube-system -f ./charts/public/external-dns/dev-values.yaml ./charts/public/external-dns --create-namespace --wait
+helm install alb -n kube-system -f ./charts/public/aws-load-balancer-controller/dev-values.yaml aws/aws-load-balancer-controller --create-namespace --wait
+helm install gpu -n gpu -f ./charts/extension/gpu/dev-values.yaml ./charts/extension/gpu --create-namespace --wait
 
 argoCDPassword=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo)
 rm -f ./argocd-password

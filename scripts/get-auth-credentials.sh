7bdd4346c6a18a33c04beb85833ec7b4256af8a6
Jiho Lee <optional.int@kakao.com>
2022-12-12 22:42:32 +0900

    fix: grafana credentials jsonpath

--- scripts/get-auth-credentials.sh
+++ scripts/get-auth-credentials.sh
@@ -10,8 +10,8 @@ export argocdPassword=$(kubectl -n argocd get secret argocd-initial-admin-secret
 
 export kialiToken=$(kubectl get secret -n istio-system $(kubectl get secret -n istio-system --no-headers -o custom-columns=":metadata.name" | grep kiali-token) -o jsonpath={.data.token} | base64 -d)
 
-export grafanaUser=$(kubectl -n monitoring get secret grafana-admin-secret -o jsonpath="{.data.admin-user}" | base64 -d; echo)
-export grafanaPassword=$(kubectl -n monitoring get secret grafana-admin-secret -o jsonpath="{.data.admin-password}" | base64 -d; echo)
+export grafanaUser=$(kubectl -n monitoring get secret grafana-admin-secret -o jsonpath="{.data.grafana-user}" | base64 -d; echo)
+export grafanaPassword=$(kubectl -n monitoring get secret grafana-admin-secret -o jsonpath="{.data.grafana-password}" | base64 -d; echo)
 
 rm -f ./auth-credentials.yaml
 envsubst < ./scripts/auth-credentials.template.yaml > ./auth-credentials.yaml
